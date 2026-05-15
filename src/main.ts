7093516ead4f6b0568310d23d155edd6bd316310
Fakihi-Yasin <yasinfakihi21@gmail.com>
2026-05-15 02:21:24 +0200

    feat: set up project environment and install dependencies

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+
+  const config = new DocumentBuilder()
+    .setTitle('Shopping API')
+    .setDescription('Shopping & Order Management API')
+    .setVersion('1.0')
+    .build();
+  SwaggerModule.setup('api', app, SwaggerModule.createDocument(app, config));
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
